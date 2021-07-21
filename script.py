532ab95731d7bd0d25543d1a5ddf258d8d88b9f4
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2021-07-21 20:58:27 +0000

    add mobile number

--- script.py
+++ script.py
@@ -28,20 +28,26 @@ container_address = soup_one.findAll('span', {'class':'cp-details-label'})
 container_mobile = soup_one.findAll('div', {'id': 'secondary-details'} )
 # contain = container_address[0]
 
-for contain in container:
-    address = contain.div.text 
+for contain in container_address:
+    address = contain.text 
     address_item = address + ', '
+    print(address_item)
 #     address_title = container_address[0].text
 #     address = container_address[1].text
 #     address_text = container.div.div.div.text
 #     print(address_title)
 #     print(address_text)
-print(address_item)
 
+# for conta in container_mobile:
+#     mobille = conta.text
+#     mob = mobille + ', '
+#     print(mob)
+
+# Getting mobile number:
 mobile = container_mobile[0].text 
-
+mobille = mobile[0:7]
 number = mobile[8:20]
-# print(mobile)
+print(mobille)
 print(number)
 
 
