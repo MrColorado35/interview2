0789cb33f9b33d240dbd0b43556914d9dbedc3de
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2021-07-21 15:03:50 +0000

    successfully avoid cloudflare and save the page to separate file

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Web scraping with Python
 
 While working on this project I was using the knowledge found in internet here: https://www.youtube.com/watch?v=XQgXKtPSzUI and here: https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
+
+Then, as the website is protected, I had to change my approach:
--- script.py
+++ script.py
@@ -1,11 +1,16 @@
 import requests
+import cloudscraper
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 my_url = 'https://bournemouth.cylex-uk.co.uk/company/parkside-motor-company-26942573.html'
 
-req = requests.get(my_url)
+scraper = cloudscraper.CloudScraper()
+# scraper = cloudscraper.create_scraper()
+# req = requests.get(my_url)
+req = scraper.get(my_url)
 
 soup_one = soup(req.content, 'html5lib')
-print(soup_one.prettify())
+# print(soup_one.prettify())
 
+print(scraper.get(soup_one.prettify()))
