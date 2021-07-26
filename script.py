0d0fbddfc40470399133d813a2ba14b34e6808cb
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2021-07-26 09:37:50 +0000

    cleaning unused or commented code

--- script.py
+++ script.py
@@ -2,42 +2,17 @@ import requests
 import cloudscraper
 from bs4 import BeautifulSoup as soup
 
-# my_url = 'https://bournemouth.cylex-uk.co.uk/company/parkside-motor-company-26942573.html'
-
-
-# scraper = cloudscraper.CloudScraper()
-# # scraper = cloudscraper.create_scraper() - alternative to the one above
-# # code for unprotected websites, where we don't need scraper:
-# # req = requests.get(my_url)
-
-# # Grabbing the page despite it's security systems:
-# req = scraper.get(my_url)
-# req.close()
-
-# soup_one = soup(req.content, 'html5lib')
-# # tests if it works:
-# # print(soup_one.h1) 
-# # print(scraper.get(soup_one.prettify()))
-
 
 def get_data_one():
     my_url = 'https://bournemouth.cylex-uk.co.uk/company/parkside-motor-company-26942573.html'
     scraper = cloudscraper.CloudScraper()
-# scraper = cloudscraper.create_scraper() - alternative to the one above
-# code for unprotected websites, where we don't need scraper:
-# req = requests.get(my_url)
 
 # Grabbing the page despite it's security systems:
     req = scraper.get(my_url)
     req.close()
 
     soup_one = soup(req.content, 'html5lib')
-# tests if it works:
-# print(soup_one.h1) 
-# print(scraper.get(soup_one.prettify()))
 
-
-    # container = soup_one.find('div', {'id': 'company-address-container'})
     container_address = soup_one.findAll('span', {'class':'cp-details-label'})
     company = soup_one.find('span', {'id': 'cntct-name'})
     comp = company.text
@@ -70,8 +45,6 @@ def get_data_one():
     number = mobile[8:20]
     f.write('\n\n' + mobille + ', ' + number + '\n\n')
 
-    # opening = soup_one.find('div', {'id': 'opening-hours-mini'})
-
     # Getting opening days:
     opening_days =  soup_one.findAll('span', {'class':'long-oh-day'})
     opening_hours = soup_one.findAll('div', {'class': 'interval-field'}, 'div.span')
@@ -89,8 +62,6 @@ def get_data_one():
                 f.write(hour + ', \n')
 
     # Getting company's description:
-
-    # description = soup_one.find('div', {'class': 'card card-left-padding'})
     description_one =  soup_one.findAll('span', {'itemprop':'name'})
     f.write("\n "+ comp + ", Can be found in following Categories: \n ,")
     for descript in description_one:
@@ -99,13 +70,8 @@ def get_data_one():
 
 
 my_url = 'https://bournemouth.cylex-uk.co.uk/car%20dealers.html'
-# def get_data_auto():
 def get_objects(my_url):
-    # my_url = 'https://bournemouth.cylex-uk.co.uk/car%20dealers.html'
     scraper = cloudscraper.CloudScraper()
-# scraper = cloudscraper.create_scraper() - alternative to the one above
-# code for unprotected websites, where we don't need scraper:
-# req = requests.get(my_url)
 
 # Grabbing the page despite it's security systems:
     req = scraper.get(my_url)
@@ -113,14 +79,7 @@ def get_objects(my_url):
 
     soup_one = soup(req.content, 'html5lib')
 
-
-
-    # addresses = soup_one.find('div', {'class': 'row address-with-medal'})
-    
     container_address = soup_one.findAll('div', {'class':'row address-with-medal'})
-    # company = soup_one.find('span', {'id': 'cntct-name'})
-    # comp = company.text
-    # container_data = soup_one.findAll('div', {'class':'col-sm-8'})
 
     for address in container_address:
         addres = address.text 
@@ -139,38 +98,15 @@ def get_companies():
         get_objects(my_url)
 
 
-
-# def get_many_objects():
-#     get_objects()
-#     my_url = 'https://bournemouth.cylex-uk.co.uk/car%20dealers-2.html'
-#     get_objects()
-#     my_url = 'https://bournemouth.cylex-uk.co.uk/car%20dealers-3.html'
-#     get_objects()
-#     my_url = 'https://bournemouth.cylex-uk.co.uk/car%20dealers-4.html'
-#     get_objects()
-#     my_url = 'https://bournemouth.cylex-uk.co.uk/car%20dealers-5.html'
-#     get_objects()
-#     my_url = 'https://bournemouth.cylex-uk.co.uk/car%20dealers-6.html'
-#     get_objects()
-#     my_url = 'https://bournemouth.cylex-uk.co.uk/car%20dealers-7.html'
-#     get_objects()
-#     my_url = 'https://bournemouth.cylex-uk.co.uk/car%20dealers-8.html'
-#     get_objects()
-#     my_url = 'https://bournemouth.cylex-uk.co.uk/car%20dealers-9.html'
-#     get_objects()
-
-
 header = '\n\n Data about other car dealers in the area: \n\n'
 header_two = '\n\n Data about other companies in the Bournemouth area: \n\n'
 
 filename = 'data_three.csv'
 f = open(filename, 'w')
-# headers = "Our data: \n"
-# f.write(headers)
+
 get_data_one()
 f.write(header)
-# get_objects()
-# get_many_objects()
+
 get_many_car_dealers()
 f.write(header_two)
 get_companies()
